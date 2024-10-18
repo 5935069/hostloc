eacc2d4eccf48dd8a6da3728cd3099989916d8ff
5935069 <71719472+5935069@users.noreply.github.com>
2024-10-18 15:21:05 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
-<?php
-return array (
-  'accounts' =>
-  array (
-    0 =>
-    array (
-      'username' => 'user1',
-      'password' => 'pass1',
-      'last_brush' => '',
-    ),
-    1 =>
-    array (
-      'username' => 'user2',
-      'password' => 'pass2',
-      'last_brush' => '',
-    ),
-  ),
-  'tg_push_key' => '',
-);
+  &它; ?菲律宾网址
+返回的  排列  (
+   'accounts' =>
+   排列  (
+     0 =>
+     排列  (
+       'username' => '淑君' ,
+       'password' => 'LIan535261993' ,
+       'last_brush' => '' ,
+     ) ,
+     1 =>
+     排列  (
+       'username' => '淑君大魔王' ,
+       'password' => 'LIan535261993' ,
+       'last_brush' => '' ,
+     ) ,
+   ) ,
+   'tg_push_key' => '2Qh9TU1VFdNXPgzk952VyjOuzxFhhZwWs1Xhh6j4Ov4=' ,
+ ) ;
